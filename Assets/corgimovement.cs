85870d7c0a2648225ffe1c12a9c838667a0a5ca5
trans-osaurus <38438534+trans-osaurus@users.noreply.github.com>
2018-10-11 11:17:43 -0400

    Player character movement is working and you can now pick up objects as long as they are tagged as an item. Going to now work on Win stages and building out the actually scavenger hunt part of the game, as well as making the movement and camera smoother.

---
+++ Assets/corgimovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corgimovement : MonoBehaviour {
+
+	//usage: put this script on a Cylinder in a closed off area
+	//intent: make this cylinder ("Corgi") move around
+	
+	
+	// Update is called once per frame
+	void Update () {
+		//step 1: define ray
+		Ray corgiRay = new Ray(transform.position, transform.forward);
+		
+		//step 2: define maximum distance for raycast
+		float maxRaycastDistance = 1f;
+		
+		//step 3: visualize
+		Debug.DrawRay(corgiRay.origin, corgiRay.direction * maxRaycastDistance, Color.yellow);
+		
+		//step 4
+		if (Physics.Raycast(corgiRay, maxRaycastDistance))
+		{
+			//randomly turn left or right?
+			int randomNumber = Random.Range(0, 100); //random number from 0-100
+			if (randomNumber < 50)
+			{//50% chance of turning left
+				transform.Rotate(0f, -90f, 0f);
+
+			}
+			else
+			{
+				transform.Rotate(0f, 90f, 0f);
+			}
+		}
+		else
+		{
+			transform.Translate(0f, 0f, Time.deltaTime);
+		}
+	}
+	
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    //this variable remembers input and passes it to physics
+    private Vector3 inputVector;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //mouselook. these r mouse deltas / differences between different frames
+        //0 when nothing is moving. this ISNT MOUSE POSITION
+        float mouseX = Input.GetAxis("Mouse X"); //horizontal mouse movement
+        float mouseY = Input.GetAxis("Mouse Y"); //vertical
+        
+        //rotate the camera based on mouse input
+        //pitch: up and down rotation (x axis) yaw = left and right rotation (Y axis) roll = rolling (z axis)
+        //first rotate body based on horizontal mouse movement
+        transform.Rotate( 0f, mouseX, 0f);
+        Camera.main.transform.Rotate(-mouseY, 0f, 0f);
+
+        
+        //WASD MOVEMENT
+        //get axiss returns a float between -1f and 1f. when you're not pressing anything it returns 0f
+        float horizontal = Input.GetAxis("Horizontal"); // a d
+        float vertical = Input.GetAxis("Vertical"); // w s
+        //apply keyboard input to position
+        //when you do this, you're teleporting. no collision detection
+        //transform.position += transform.forward * vertical * moveSpeed; //forward
+        //transform.position += transform.right * horizontal * moveSpeed; //strafe
+      
+
+        //collecting data. remember to use += so it means 'also equals' i guess??
+        inputVector = transform.forward * vertical;
+        inputVector += transform.right * horizontal;
+        
+
+    }
+//runs every physics frame (a different framerate than input or rendering or anything else)
+//PUT ALL PHYSICS CODE IN FIXEDUPDATE
+    void FixedUpdate()
+    {
+        //override object's velocity with desired inputVector direction
+        GetComponent<Rigidbody>().velocity = inputVector * moveSpeed + Physics.gravity * 0.5f;
+    }
+}
